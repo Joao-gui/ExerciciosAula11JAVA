6c77afa594521e2665d8b41688d6c4b153ba503d
João Guilherme <jaum.gui001@gmail.com>
2019-07-17 21:22:45 -0300

    Termino Exercício de retornar o menor valor Double

--- src/com/db1/db1start/NumeroDouble.java
+++ src/com/db1/db1start/NumeroDouble.java
@@ -2,10 +2,13 @@ package com.db1.db1start;
 
 public class NumeroDouble {
 	public static void main (String[] args) {
-		Integer menorDe2Valores = MenorMaior.operacao(30, 87);
-		Integer menorDe3Valores = MenorMaior.operacao3Numeros(30, 87, 64);
-		System.out.println("O menor numero entre 2 é: " +menorDe2Valores +"\n"
-				+"Menor entre 3 valores é: "+menorDe3Valores);
+		Double valor1 = 87.5;
+		Double valor2 = 55.47;
+		Double valor3 = 48.2;
+		Double menorDe2Valores = MenorMaior.operacao(valor1, valor2);
+		Double menorDe3Valores = MenorMaior.operacao3Numeros(valor1, valor2, valor3);
+		System.out.println("O menor numero entre " +valor1+" e "+valor2 +" é " +menorDe2Valores +"\n"
+				+"Menor entre "+valor1+ ", "+valor2+" e "+valor3+" é: "+menorDe3Valores);
 		
 	}
 
