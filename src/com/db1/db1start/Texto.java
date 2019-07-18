396e2405831d608203061778c834dc20957e8c9e
João Guilherme <jaum.gui001@gmail.com>
2019-07-17 21:47:12 -0300

    Commit do exercício de voltar o texto com Letras MAIUSCULAS e MINUSCULAS

---
+++ src/com/db1/db1start/Texto.java
@@ -0,0 +1,11 @@
+package com.db1.db1start;
+
+public class Texto {
+	public static void main (String[] args) {
+		String textoMaiusculo = "exercicio 2 aula java db1 start minusculo para maiusculo.";
+		String textoMinusculo = "EXERCICIO 2 AULA JAVA DB1 START MAIUSCULO PARA MINUSCULO.";
+		System.out.println(textoMaiusculo.toUpperCase());
+		System.out.println(textoMinusculo.toLowerCase());
+	}
+
+}
